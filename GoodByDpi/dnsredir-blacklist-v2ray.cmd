75f2ff8b9369c3b957daeb59d2a6268094bcb98a
Kaan Alper <halis.meryem2005@gmail.com>
2025-01-04 12:30:54 +0300

    bug fix

--- GoodByDpi/dnsredir-blacklist-v2ray.cmd
+++ GoodByDpi/dnsredir-blacklist-v2ray.cmd
@@ -2,13 +2,11 @@
 PUSHD "%~dp0"
 set _arch=x86
 
-nircmd exec hide V2RAY\v2ray.exe run
-
 IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set _arch=x86_64)
 IF DEFINED PROCESSOR_ARCHITEW6432 (set _arch=x86_64)
 PUSHD "%_arch%"
 
-nircmd exec hide goodbyedpi.exe -5 --dns-addr 77.88.8.8 --dns-port 1253 --dnsv6-addr 2a02:6b8::feed:0ff --dnsv6-port 1253 --blacklist ..\blacklist.txt
+start "" goodbyedpi.exe -5 --dns-addr 77.88.8.8 --dns-port 1253 --dnsv6-addr 2a02:6b8::feed:0ff --dnsv6-port 1253 --blacklist ..\blacklist.txt
 
 POPD
 POPD
